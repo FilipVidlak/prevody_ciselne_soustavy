53f34a7c4727e8ee2613b9b51af0b30ebf019997
Filip Vidlák <vidlakf.03@spst.eu>
2022-05-15 20:47:40 +0200

    Přidejte soubory projektu.

---
+++ prevody_ciselne_soustavy/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace prevody_ciselne_soustavy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(318, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(318, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 36);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(318, 318);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 36);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(92, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BinToDec and\r\n    BinToHex";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(87, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DecToBin and   \r\n    DecToHex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(87, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "HexToBin and \r\n    HexToDec";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(118, 417);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(493, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Výsledek převodu najdete ve zbylých dvou TextBoxech";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.Aqua;
+            this.label6.Location = new System.Drawing.Point(45, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 22);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "INFO:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(582, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 87);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Vyčistit TextBoxy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Binary/Decimal/Hexadecimal CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ prevody_ciselne_soustavy/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prevody_ciselne_soustavy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int BinToDec(string hodnota)
+        {
+            char[] cisla = hodnota.ToCharArray();
+            Array.Reverse(cisla); // zprava od nejmensiho po nejvetsi
+            //dal beru index a davam dvojku na i-tou
+            // pokud je i rovno nule, je to 2 na nultou, pricitam jednicku
+            int soucet = 0;
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                if (cisla[i] == '1')
+                {
+                    if (i == 0) { soucet++; }
+                    else { soucet += (int)Math.Pow(2, i); }
+                }
+            }
+            return soucet;
+        }
+        private string DecToBin(int cislo)
+        {
+            string vysledek = "";
+            int d = cislo;
+            int a = -1;
+            int b = -1;
+            while (a != 1)
+            {
+                b = d % 2;
+                a = d / 2;
+                d = a;
+                vysledek += b.ToString();
+            }
+            vysledek += a.ToString();
+            return vysledek;
+
+        }
+        private int HexToDec(string value)
+        {
+            int vysledek = 0;
+            int pocet = value.Length - 1;
+            for (int i = 0; i < value.Length; i++)
+            {
+                int x = 0;
+                switch (value[i])
+                {
+                    case 'A': x = 10; break;
+                    case 'B': x = 11; break;
+                    case 'C': x = 12; break;
+                    case 'D': x = 13; break;
+                    case 'E': x = 14; break;
+                    case 'F': x = 15; break;
+                    default: x = -48/*ascii kod*/ + (int)value[i]; break;
+                }
+                vysledek += x * (int)(Math.Pow(16, pocet));
+            }
+            return vysledek;
+        }
+        private string DecToHex(int x)
+        {
+            string vys = "";
+
+            while (x != 0)
+            {
+                if ((x % 16) < 10)
+                    vys = x % 16 + vys;
+                else
+                {
+                    string znaky = "";
+
+                    switch (x % 16)
+                    {
+                        case 10: znaky = "A"; break;
+                        case 11: znaky = "B"; break;
+                        case 12: znaky = "C"; break;
+                        case 13: znaky = "D"; break;
+                        case 14: znaky = "E"; break;
+                        case 15: znaky = "F"; break;
+                    }
+
+                    vys = znaky + vys;
+                }
+
+                x = x / 16;
+            }
+
+            return vys;
+        }
+        private string HexToBin(string hex)
+        {
+            string bin = "";
+            bin = (Convert.ToInt32(hex, 16), 2).ToString();
+            return bin;
+        }
+        private string BinToHex(string bin)
+        {
+            int cast = bin.Length % 4;
+            if (cast != 0)
+                bin = new string('0', 4 - cast) + bin;
+
+            string vystup = "";
+
+            for (int i = 0; i <= bin.Length - 4; i += 4)
+            {
+                vystup += string.Format("{0:X}", Convert.ToByte(bin.Substring(i, 4), 2));
+            }
+            return vystup;
+        }
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar != '0' && e.KeyChar != '1') { e.Handled = true; }
+                int vystup = BinToDec(textBox1.Text);
+                textBox2.Text = vystup.ToString();
+                textBox3.Text = BinToHex(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nastala chyba!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (int.Parse(e.KeyChar.ToString()) < 0 && int.Parse(e.KeyChar.ToString()) > 9)
+                {
+                    e.Handled = true;
+                }
+                string vystup = DecToHex(int.Parse(textBox2.Text));
+                textBox3.Text = vystup;
+                textBox1.Text = DecToBin(int.Parse(textBox2.Text));
+            }
+            catch
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar < 48 || (int)e.KeyChar > 70 || (int)e.KeyChar > 57 && (int)e.KeyChar < 65)
+            {
+                e.Handled = true;
+            }
+            textBox1.Text = HexToBin(textBox3.Text);
+            int vystup = HexToDec(textBox1.Text);
+            textBox2.Text = vystup.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            Application.Restart();
+        }
+    }
+}
